307015f935478c9f9f2705cd260b97e09ea4a331
Prathyoosha <chayap@oregonstate.edu>
2019-01-12 16:10:37 -0800

    Test for IRIS fix

--- software/environment/rover/startup/startup.sh
+++ software/environment/rover/startup/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/ros/kinetic/setup.bash
 source /home/nvidia/catkin_workspace/devel/setup.bash
-/home/nvidia/Github/Rover_2017_2018/software/environment/rover/auto_poweroff/auto_poweroff.py &
+/home/nvidia/Github/Rover_2017_2018/software/environment/rover/auto_poweroff/auto_poweroff_test.py &
 roslaunch rover_main rover.launch
 exec bash
