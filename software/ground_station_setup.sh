78911daa4662efcdafccefdcb94448a6f487f84c
Scarabax <23585793+Scarabax@users.noreply.github.com>
2019-10-23 16:10:43 -0700

    Updated Repository Name
    Changed 2018_2019 2019_2020

--- software/ground_station_setup.sh
+++ software/ground_station_setup.sh
@@ -26,7 +26,7 @@ catkin_workspace_path="$HOME/$catkin_workspace_dir"
 catkin_src_path="$catkin_workspace_path/src"
 
 # Get the rover software directory
-github_rover_repo_dir="Github/Rover_2018_2019"
+github_rover_repo_dir="Github/Rover_2019_2020"
 github_rover_packages_path="$HOME/$github_rover_repo_dir/software/ros_packages"
 
 # Remove existing symbolic links if necessary
