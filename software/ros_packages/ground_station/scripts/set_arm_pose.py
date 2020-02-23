16cca616fb6e163a758dcd29da0bd6f321d095f5
Scarabax <amornsrisaja@outlook.com>
2020-02-23 11:52:15 -0800

    addded OMPL as STOMp pre-prcessor, added a new launch file under rover_main for arm with moveit, added to scripts under the rover_arm folder (warning: scripts are a work in progress)

--- software/ros_packages/ground_station/scripts/set_arm_pose.py
+++ software/ros_packages/ground_station/scripts/set_arm_pose.py
@@ -16,9 +16,9 @@ group = moveit_commander.MoveGroupCommander('arm')
 
 pose_goal = geometry_msgs.msg.Pose()
 pose_goal = group.get_random_pose()
-pose_goal.pose.position.x = 0.4
-pose_goal.pose.position.y = -0.3
-pose_goal.pose.position.z = 0.8
+pose_goal.pose.position.x = 0.2
+pose_goal.pose.position.y = 0.2
+pose_goal.pose.position.z = 0.2
 pose_goal.pose.orientation.x = 0
 pose_goal.pose.orientation.y = 0
 pose_goal.pose.orientation.z = 0
