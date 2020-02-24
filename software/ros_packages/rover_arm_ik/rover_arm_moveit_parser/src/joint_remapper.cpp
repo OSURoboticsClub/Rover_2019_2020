407f9563a8581b5de98c952ba8ab03c521525ef8
Scarabax <amornsrisaja@outlook.com>
2020-02-23 22:57:07 -0800

    joint_remapper.cpp now working for tracking arm position and publish as int64 for each joint

--- software/ros_packages/rover_arm_ik/rover_arm_moveit_parser/src/joint_remapper.cpp
+++ software/ros_packages/rover_arm_ik/rover_arm_moveit_parser/src/joint_remapper.cpp
@@ -1,22 +1,24 @@
 #include<ros/ros.h>
-#include<sensor_msgs/JointStates.h>
+#include<sensor_msgs/JointState.h>
 #include<rover_arm_moveit_parser/ArmJointStates.h>
 #include<math.h>
 
-rover_arm::ArmJointState arm_steps;
-rover_arm::ArmJointState total;
+rover_arm_moveit_parser::ArmJointStates arm_steps;
+rover_arm_moveit_parser::ArmJointStates total;
 
-int stepsPerRevolution[6] = {32800,18000,72000,3280,14400,0};  // microsteps/revolution (using 16ths) from observation, for each motor
+int stepsPerRevolution[6] = {32800,18000,72000,3280,14400,0};  //microsteps/revolution (using 16ths) from observation, for each motor
+//TODO: check values for stepsPerRevolution
+
 int joint_status = 0; 
 double cur_angle[6]; //
 int joint_step[6]; //
 double prev_angle[6] = {0,0,0,0,0,0}; //
 double init_angle[6] = {0,0,0,0,0,0}; //
-double total_steps[6] = {0,0,0,0,0,0}; //
+double total_steps[6] = {0,0,0,0,0,0}; //track number of steps
 int count = 0;
 
-void parse_joints(const sensor_msgs::JointState& arm)
-{/*
+void parse_joints(const sensor_msgs::JointState& arm){
+//rosmsg show sensor_msg/JointState for info
   if (count==0){
     prev_angle[0] = arm.position[0];
     prev_angle[1] = arm.position[1];
@@ -36,46 +38,44 @@ void parse_joints(const sensor_msgs::JointState& arm)
   ROS_INFO_STREAM("Received /move_group/fake_controller_joint_states");
 
   
-  arm_steps.position1 = (int)((arm.position[0]-prev_angle[0])*stepsPerRevolution[0]/(2*M_PI));
-  arm_steps.position2 = (int)((arm.position[1]-prev_angle[1])*stepsPerRevolution[1]/(2*M_PI));
-  arm_steps.position3 = (int)((arm.position[2]-prev_angle[2])*stepsPerRevolution[2]/(2*M_PI));
-  arm_steps.position4 = (int)((arm.position[3]-prev_angle[3])*stepsPerRevolution[3]/(2*M_PI));
-  arm_steps.position5 = (int)((arm.position[4]-prev_angle[4])*stepsPerRevolution[4]/(2*M_PI));
-  arm_steps.position6 = (int)((arm.position[5]-prev_angle[5])*stepsPerRevolution[5]/(2*M_PI));
+  arm_steps.base = (int)((arm.position[0]-prev_angle[0])*stepsPerRevolution[0]/(2*M_PI));
+  arm_steps.shoulder = (int)((arm.position[1]-prev_angle[1])*stepsPerRevolution[1]/(2*M_PI));
+  arm_steps.elbow = (int)((arm.position[2]-prev_angle[2])*stepsPerRevolution[2]/(2*M_PI));
+  arm_steps.roll = (int)((arm.position[3]-prev_angle[3])*stepsPerRevolution[3]/(2*M_PI));
+  arm_steps.wrist_pitch = (int)((arm.position[4]-prev_angle[4])*stepsPerRevolution[4]/(2*M_PI));
+  arm_steps.wrist_roll = (int)((arm.position[5]-prev_angle[5])*stepsPerRevolution[5]/(2*M_PI));
 
-  ROS_INFO_NAMED("test", "arm_steps.position5 #2: %d", arm_steps.position5);
-  */
-/*  if (count!=0){
+  ROS_INFO_NAMED("test", "arm_steps.wrist_pitch #2: %d", arm_steps.wrist_pitch);
+
+  if (count!=0){
     prev_angle[0] = arm.position[0];
     prev_angle[1] = arm.position[1];
     prev_angle[2] = arm.position[2];
     prev_angle[3] = arm.position[3];
     prev_angle[4] = arm.position[4];
     prev_angle[5] = arm.position[5];
-  }*/
+  }
 
-  /*
-  total.position1 += arm_steps.position1;
-  total.position2 += arm_steps.position2;
-  total.position3 += arm_steps.position3;
-  total.position4 += arm_steps.position4;
-  total.position5 += arm_steps.position5;
-  */
+  total.base += arm_steps.base;
+  total.shoulder += arm_steps.shoulder;
+  total.elbow += arm_steps.elbow;
+  total.roll += arm_steps.roll;
+  total.wrist_pitch += arm_steps.wrist_pitch;
+  total.wrist_roll += arm_steps.wrist_roll; 
 
-  //ROS_INFO_NAMED("test", "total_steps[4]: %f, total: %d", total_steps[4], total.position5);
-  //ROS_INFO_NAMED("test", "arm_steps.position5 #3: %d", arm_steps.position5);
-/*
+  ROS_INFO_NAMED("test", "total_steps[4]: %f, total: %d", total_steps[4], total.wrist_pitch);
+  ROS_INFO_NAMED("test", "arm_steps.wrist_pitch #3: %d", arm_steps.wrist_pitch);
   ROS_INFO_STREAM("Done conversion to /joint_steps");
   joint_status = 1;
-  count=1;*/
+  count=1;
 }
 
 int main(int argc, char **argv){
    ros::init(argc, argv,"joint_parser");
    ros::NodeHandle nh;
    //ROS_INFO_STREAM("In main function");
-   ros::Subscriber sub = nh.subscribe("/move_group/fake_controller_joint_states",1000,parse_joints);
-   ros::Publisher pub = nh.advertise<moveo_moveit::ArmJointState>("joint_steps",50);
+   ros::Subscriber sub = nh.subscribe("/joint_states",1000,parse_joints);
+   ros::Publisher pub = nh.advertise<rover_arm_moveit_parser::ArmJointStates>("joint_steps",50);
   
    ros::Rate loop_rate(20);
    
