2e1a463522d2e66b72ee873c1870302ae3f84f72
Scarabax <amornsrisaja@outlook.com>
2020-02-25 01:12:20 -0800

    Why are we still here? Just to suffer?

--- software/ros_packages/rover_arm_ik/joint_parser/src/joint_remapper.cpp
+++ software/ros_packages/rover_arm_ik/joint_parser/src/joint_remapper.cpp
@@ -3,18 +3,18 @@
 #include<rover_arm_moveit_parser/ArmJointStates.h>
 #include<math.h>
 
-rover_arm_moveit_parser::ArmJointStates arm_steps;
-rover_arm_moveit_parser::ArmJointStates total;
+joint_parser::ArmJointStates arm_trajectory;
+joint_parser::ArmJointStates total;
 
 int stepsPerRevolution[6] = {1000,1000,1000,1000,1000,1000}; //microsteps/revolution (using 16ths) from observation, for each motor
 
 
 int joint_status = 0; 
-float cur_angle[6]; //
+double cur_angle[6]; //
 int joint_step[6]; //
-float prev_angle[6] = {0,0,0,0,0,0}; //
-float init_angle[6] = {0,0,0,0,0,0}; //
-float total_steps[6] = {0,0,0,0,0,0}; //track number of steps
+double prev_angle[6] = {0,0,0,0,0,0}; //
+double init_angle[6] = {0,0,0,0,0,0}; //
+double total_steps[6] = {0,0,0,0,0,0}; //track number of steps
 int count = 0;
 
 void parse_joints(const sensor_msgs::JointState& arm){
@@ -44,12 +44,12 @@ void parse_joints(const sensor_msgs::JointState& arm){
   ROS_INFO_STREAM("Received /joint_states");
 
   
-  arm_steps.base_roll = (int)((arm.position[0]-prev_angle[0])*stepsPerRevolution[0]/(2*M_PI));
-  arm_steps.shoulder_pitch = (int)((arm.position[1]-prev_angle[1])*stepsPerRevolution[1]/(2*M_PI));
-  arm_steps.elbow_pitch = (int)((arm.position[2]-prev_angle[2])*stepsPerRevolution[2]/(2*M_PI));
-  arm_steps.elbow_roll = (int)((arm.position[3]-prev_angle[3])*stepsPerRevolution[3]/(2*M_PI));
-  arm_steps.wrist_pitch = (int)((arm.position[4]-prev_angle[4])*stepsPerRevolution[4]/(2*M_PI));
-  arm_steps.wrist_roll = (int)((arm.position[5]-prev_angle[5])*stepsPerRevolution[5]/(2*M_PI));
+  arm_trajectory.base_roll = (int)((arm.position[0]-prev_angle[0])*stepsPerRevolution[0]/(2*M_PI));
+  arm_trajectory.shoulder_pitch = (int)((arm.position[1]-prev_angle[1])*stepsPerRevolution[1]/(2*M_PI));
+  arm_trajectory.elbow_pitch = (int)((arm.position[2]-prev_angle[2])*stepsPerRevolution[2]/(2*M_PI));
+  arm_trajectory.elbow_roll = (int)((arm.position[3]-prev_angle[3])*stepsPerRevolution[3]/(2*M_PI));
+  arm_trajectory.wrist_pitch = (int)((arm.position[4]-prev_angle[4])*stepsPerRevolution[4]/(2*M_PI));
+  arm_trajectory.wrist_roll = (int)((arm.position[5]-prev_angle[5])*stepsPerRevolution[5]/(2*M_PI));
 
   if (count!=0){
     prev_angle[0] = arm.position[0];
@@ -60,12 +60,12 @@ void parse_joints(const sensor_msgs::JointState& arm){
     prev_angle[5] = arm.position[5];
   }
 
-  total.base_roll += arm_steps.base_roll;
-  total.shoulder_pitch += arm_steps.shoulder_pitch;
-  total.elbow_pitch += arm_steps.elbow_pitch;
-  total.elbow_roll += arm_steps.elbow_roll;
-  total.wrist_pitch += arm_steps.wrist_pitch;
-  total.wrist_roll += arm_steps.wrist_roll; 
+  total.base_roll += arm_trajectory.base_roll;
+  total.shoulder_pitch += arm_trajectory.shoulder_pitch;
+  total.elbow_pitch += arm_trajectory.elbow_pitch;
+  total.elbow_roll += arm_trajectory.elbow_roll;
+  total.wrist_pitch += arm_trajectory.wrist_pitch;
+  total.wrist_roll += arm_trajectory.wrist_roll; 
 
   joint_status = 1;
   count=1;*/
