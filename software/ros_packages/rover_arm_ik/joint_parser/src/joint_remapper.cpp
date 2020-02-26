667d67a79e985e5abf24c34eca13e6762a1b47e6
Scarabax <amornsrisaja@outlook.com>
2020-02-25 17:36:48 -0800

    fixed joint_parser, will continue to edit

--- software/ros_packages/rover_arm_ik/joint_parser/src/joint_remapper.cpp
+++ software/ros_packages/rover_arm_ik/joint_parser/src/joint_remapper.cpp
@@ -6,7 +6,7 @@
 joint_parser::ArmJointStates arm_trajectory;
 joint_parser::ArmJointStates end_trajectory;
 
-int stepsPerRevolution[6] = {1000,1000,1000,1000,1000,1000};
+int stepsPerRevolution[6] = {1, 1, 1, 1, 1, 1};
 
 int joint_status = 0; 
 double cur_angle[6]; //
@@ -17,57 +17,57 @@ double end_trajectory_steps[6] = {0,0,0,0,0,0}; //track number of steps
 int count = 0;
 
 void parse_joints(const sensor_msgs::JointState& arm){
-  end_trajectory.base_roll = arm.position[0];
-  end_trajectory.shoulder_pitch = arm.position[1];
-  end_trajectory.elbow_pitch = arm.position[2];
-  end_trajectory.elbow_roll = arm.position[3];
-  end_trajectory.wrist_pitch = arm.position[4];
-  end_trajectory.wrist_roll  = arm.position[5];
-   /*
-  if (count==0){
-    prev_angle[0] = arm.position[0];
-    prev_angle[1] = arm.position[1];
-    prev_angle[2] = arm.position[2];
-    prev_angle[3] = arm.position[3];
-    prev_angle[4] = arm.position[4];
-    prev_angle[5] = arm.position[5];
+   end_trajectory.base_roll = arm.position[0];
+   end_trajectory.shoulder_pitch = arm.position[1];
+   end_trajectory.elbow_pitch = arm.position[2];
+   end_trajectory.elbow_roll = arm.position[3];
+   end_trajectory.wrist_pitch = arm.position[4];
+   end_trajectory.wrist_roll  = arm.position[5];
+ 
 
-    init_angle[0] = arm.position[0];
-    init_angle[1] = arm.position[1];
-    init_angle[2] = arm.position[2];
-    init_angle[3] = arm.position[3];
-    init_angle[4] = arm.position[4];
-    init_angle[5] = arm.position[5];
-  }
+   if (count==0){
+      prev_angle[0] = arm.position[0];
+      prev_angle[1] = arm.position[1];
+      prev_angle[2] = arm.position[2];
+      prev_angle[3] = arm.position[3];
+      prev_angle[4] = arm.position[4];
+      prev_angle[5] = arm.position[5];
 
-  ROS_INFO_STREAM("Received /joint_states");
+      init_angle[0] = arm.position[0];
+      init_angle[1] = arm.position[1];
+      init_angle[2] = arm.position[2];
+      init_angle[3] = arm.position[3];
+      init_angle[4] = arm.position[4];
+      init_angle[5] = arm.position[5];
+   }
 
+   ROS_INFO_STREAM("Received /joint_states");
   
-  arm_trajectory.base_roll = (int)((arm.position[0]-prev_angle[0])*stepsPerRevolution[0]/(2*M_PI));
-  arm_trajectory.shoulder_pitch = (int)((arm.position[1]-prev_angle[1])*stepsPerRevolution[1]/(2*M_PI));
-  arm_trajectory.elbow_pitch = (int)((arm.position[2]-prev_angle[2])*stepsPerRevolution[2]/(2*M_PI));
-  arm_trajectory.elbow_roll = (int)((arm.position[3]-prev_angle[3])*stepsPerRevolution[3]/(2*M_PI));
-  arm_trajectory.wrist_pitch = (int)((arm.position[4]-prev_angle[4])*stepsPerRevolution[4]/(2*M_PI));
-  arm_trajectory.wrist_roll = (int)((arm.position[5]-prev_angle[5])*stepsPerRevolution[5]/(2*M_PI));
-
-  if (count!=0){
-    prev_angle[0] = arm.position[0];
-    prev_angle[1] = arm.position[1];
-    prev_angle[2] = arm.position[2];
-    prev_angle[3] = arm.position[3];
-    prev_angle[4] = arm.position[4];
-    prev_angle[5] = arm.position[5];
-  }
-
-  end_trajectory.base_roll += arm_trajectory.base_roll;
-  end_trajectory.shoulder_pitch += arm_trajectory.shoulder_pitch;
-  end_trajectory.elbow_pitch += arm_trajectory.elbow_pitch;
-  end_trajectory.elbow_roll += arm_trajectory.elbow_roll;
-  end_trajectory.wrist_pitch += arm_trajectory.wrist_pitch;
-  end_trajectory.wrist_roll += arm_trajectory.wrist_roll; 
-
-  joint_status = 1;
-  count=1;*/
+   arm_trajectory.base_roll = (int)((arm.position[0]-prev_angle[0])); //*stepsPerRevolution[0]/(2*M_PI));
+   arm_trajectory.shoulder_pitch = (int)((arm.position[1]-prev_angle[1]));//*stepsPerRevolution[1]/(2*M_PI));
+   arm_trajectory.elbow_pitch = (int)((arm.position[2]-prev_angle[2]));//*stepsPerRevolution[2]/(2*M_PI));
+   arm_trajectory.elbow_roll = (int)((arm.position[3]-prev_angle[3]));//*stepsPerRevolution[3]/(2*M_PI));
+   arm_trajectory.wrist_pitch = (int)((arm.position[4]-prev_angle[4]));//*stepsPerRevolution[4]/(2*M_PI));
+   arm_trajectory.wrist_roll = (int)((arm.position[5]-prev_angle[5]));//*stepsPerRevolution[5]/(2*M_PI));
+ 
+   if (count!=0){
+      prev_angle[0] = arm.position[0]; 
+      prev_angle[1] = arm.position[1];
+      prev_angle[2] = arm.position[2];
+      prev_angle[3] = arm.position[3];
+      prev_angle[4] = arm.position[4];
+      prev_angle[5] = arm.position[5];
+   }
+ 
+   end_trajectory.base_roll += arm_trajectory.base_roll;
+   end_trajectory.shoulder_pitch += arm_trajectory.shoulder_pitch;
+   end_trajectory.elbow_pitch += arm_trajectory.elbow_pitch;
+   end_trajectory.elbow_roll += arm_trajectory.elbow_roll;
+   end_trajectory.wrist_pitch += arm_trajectory.wrist_pitch;
+   end_trajectory.wrist_roll += arm_trajectory.wrist_roll; 
+ 
+   joint_status = 1;
+   count++; 
 }
 
 int main(int argc, char **argv){
@@ -80,7 +80,7 @@ int main(int argc, char **argv){
    
    while (ros::ok()){
       if(joint_status==1){
-   	 joint_status = 0;
+   	 joint_status=0;
    	 pub.publish(end_trajectory); //publish values 
 	 ROS_INFO_STREAM("Published to /joint_states_remap");
       }
